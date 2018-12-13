80aeb911d48d4ab56d9c4a0ba25866573524ce0b
Minh-Tri Le <lmtri1995@gmail.com>
2018-12-13 18:13:10 +0700

    Add Topbar Filter Component

--- frontend_chart/src/shared/prop-types/ReducerProps.js
+++ frontend_chart/src/shared/prop-types/ReducerProps.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const {
     string, shape,
@@ -7,6 +8,11 @@ const {
 export const SidebarProps = shape({
     show: PropTypes.bool,
     collapse: PropTypes.bool,
+});
+
+export const GlobalFilterProps = shape({
+    startDate: PropTypes.instanceOf(moment),
+    endDate: PropTypes.instanceOf(moment),
 });
 
 export const ThemeProps = shape({

5d38b459d191c4b8275e334310a5f00616fe8c41
Minh-Tri Le <lmtri1995@gmail.com>
2019-02-15 13:37:04 +0700

    Remove Report Date Filter Action in Reducer

--- frontend_chart/src/redux/actions/globalDateFilterActions.js
+++ frontend_chart/src/redux/actions/globalDateFilterActions.js
@@ -1,13 +1,4 @@
-export const REPORT_DATE_FILTER = 'REPORT_DATE_FILTER';
 export const ANALYSIS_DATE_FILTER = 'ANALYSIS_DATE_FILTER';
-
-export function changeReportDateFilter(startDate, endDate) {
-    return {
-        type: REPORT_DATE_FILTER,
-        startDate: startDate,
-        endDate: endDate,
-    };
-}
 
 export function changeAnalysisDateFilter(startDate, endDate) {
     return {

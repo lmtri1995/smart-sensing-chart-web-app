f889acb9f4abea70074d7ede64e1321788cc48b3
Minh-Tri Le <lmtri1995@gmail.com>
2019-02-21 10:28:20 +0700

    Reformat code of Doughnut Chart Component in Report Page

--- frontend_chart/src/containers/Charts/ChartJS/components/RamdomAnimatedDoughnut.jsx
+++ frontend_chart/src/containers/Charts/ChartJS/components/RamdomAnimatedDoughnut.jsx
@@ -1,66 +1,65 @@
-
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
 import {Doughnut} from 'react-chartjs-2';
 
 // some of this code is a variation on https://jsfiddle.net/cmyker/u6rr5moq/
 var originalDoughnutDraw = Chart.controllers.doughnut.prototype.draw;
 Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
-  draw: function() {
-    originalDoughnutDraw.apply(this, arguments);
-    
-    var chart = this.chart.chart;
-    var ctx = chart.ctx;
-    var width = chart.width;
-    var height = chart.height;
+    draw: function () {
+        originalDoughnutDraw.apply(this, arguments);
 
-    var fontSize = (height / 114).toFixed(2);
-    ctx.font = fontSize + "em Verdana";
-    ctx.textBaseline = "middle";
+        var chart = this.chart.chart;
+        var ctx = chart.ctx;
+        var width = chart.width;
+        var height = chart.height;
 
-    var text = chart.config.data.text,
-        textX = Math.round((width - ctx.measureText(text).width) / 2),
-        textY = height / 2;
+        var fontSize = (height / 114).toFixed(2);
+        ctx.font = fontSize + "em Verdana";
+        ctx.textBaseline = "middle";
 
-    ctx.fillText(text, textX, textY);
-  }
+        var text = chart.config.data.text,
+            textX = Math.round((width - ctx.measureText(text).width) / 2),
+            textY = height / 2;
+
+        ctx.fillText(text, textX, textY);
+    }
 });
 
 const data = {
-	labels: [
-		'Red',
-		'Yellow'
-	],
-	datasets: [{
-		data: [50, 100],
-		backgroundColor: [
-		'#36A2EB',
-		'#FFCE56'
-		],
-		hoverBackgroundColor: [
-		'#36A2EB',
-		'#FFCE56'
-		]
-	}],
-  text: '23%'
+    labels: [
+        'Red',
+        'Yellow'
+    ],
+    datasets: [{
+        data: [50, 100],
+        backgroundColor: [
+            '#36A2EB',
+            '#FFCE56'
+        ],
+        hoverBackgroundColor: [
+            '#36A2EB',
+            '#FFCE56'
+        ]
+    }],
+    text: '23%'
 };
 const options = {
     cutoutPercentage: 80,
     legend: {
-        display:false
+        display: false
     },
     zoom: {
         enabled: true,
         mode: 'xy'
     }
 };
+
 class RandomAnimatedDoughnutLong extends Component {
 
-  render() {
-    return (
-        <Doughnut height={200} data={data} options={options} />
-    );
-  }
-};
+    render() {
+        return (
+            <Doughnut height={200} data={data} options={options}/>
+        );
+    }
+}
 
 export default RandomAnimatedDoughnutLong;

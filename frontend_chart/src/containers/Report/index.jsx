9073d3ae923cdc3cc079d7ac1f1f79d6b6bf9f05
Minh-Tri Le <lmtri1995@gmail.com>
2019-02-26 16:48:48 +0700

    Remove redundant comments

--- frontend_chart/src/containers/Report/index.jsx
+++ frontend_chart/src/containers/Report/index.jsx
@@ -25,30 +25,6 @@ class ReportPage extends Component {
 
     render() {
         return (
-            // <div className="container report">
-            //         <div className="row">
-
-            //             <div className="col-10">
-            //                 <h3>Line 1111</h3>
-            //             </div>
-            //             <div className="col-2">
-            //                 <div class="btn-group" >
-            //                     <button type="button" class="btn btn-primary">Productivity</button>
-            //                     <button type="button" class="btn btn-secondary">Defect</button>
-            //                 </div>
-            //             </div>
-
-            //         </div>
-            //         <div className="row">
-
-            //             <div className="col-9">
-            //             <DefectRate />
-            //             </div>
-            //             <div className="col-3">
-            //             <DefectRateOverview />
-            //             </div>
-            //         </div>
-            // </div>
             <div className="container report">
                 <div className="row">
                     <div className="col-10">

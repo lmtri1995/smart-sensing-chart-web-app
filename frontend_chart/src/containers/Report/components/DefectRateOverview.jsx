839c800473dd2e9f82e0ad7c332c17d4f51f7054
Minh-Tri Le <lmtri1995@gmail.com>
2019-02-27 08:43:41 +0700

    Remove stale api route in Defect Rate Overview Component

--- frontend_chart/src/containers/Report/components/DefectRateOverview.jsx
+++ frontend_chart/src/containers/Report/components/DefectRateOverview.jsx
@@ -1,44 +1,13 @@
 import React, {Component} from "react";
 import DoughnutChart from "../../Charts/ChartJS/components/DoughnutChart";
-import API from "../../../services/api";
 
 export default class DefectRateOverview extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            dataArray: null
-        };
-
-        let param = {
-            from_workdate: "20190202",
-            to_workdate: "20190202",
-        };
-        API('api/os/defectRateOverview', 'POST', param)
-            .then((response) => {
-                if (response.data.success) {
-                    let dataArray = response.data.data;
-
-                    let dataToShow = [];
-                    for (let i = 1; i <= 4; ++i) {
-                        dataToShow.push(dataArray[0][`DEFECT_COUNT${i}`]);
-                    }
-                    
-                    this.setState({
-                        dataArray: dataToShow,
-                    });
-                }
-            })
-            .catch((err) => console.log('err:', err))
-    }
-
     render() {
         return (
             <div className="report-main">
                 <div className="col-12"><h4>Defect Rate Overview</h4></div>
                 <div className="col-12 report-item">
-                    <DoughnutChart data={this.state.dataArray}/>
+                    <DoughnutChart/>
                 </div>
             </div>
         )

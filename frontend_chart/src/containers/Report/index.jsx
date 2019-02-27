a428a6f37745ff0e6b57cbeed0667170da3301f2
Minh-Tri Le <lmtri1995@gmail.com>
2019-02-27 08:38:25 +0700

    Get Production Rate and Defect Rate from Server in Report Page, format the response data for showing on the charts & log the results in the console

--- frontend_chart/src/containers/Report/index.jsx
+++ frontend_chart/src/containers/Report/index.jsx
@@ -4,6 +4,7 @@ import DefectRateOverview from './components/DefectRateOverview';
 import ProductionRate from './components/ProductionRate';
 import ProductionRateOverview from './components/ProductionRateOverview';
 import {TabContent, TabPane} from 'reactstrap';
+import API from "../../services/api";
 
 class ReportPage extends Component {
     constructor(props) {
@@ -11,8 +12,83 @@ class ReportPage extends Component {
 
         this.toggle = this.toggle.bind(this);
         this.state = {
-            activeTab: '1'
+            activeTab: '1',
+            dateLabels: null,
+            productionRate: null,
+            defectByTypeOverTime: null,
         };
+
+        let param = {
+            from_workdate: "20190216",
+            to_workdate: "20190225",
+        };
+        API('api/os/productionRate', 'POST', param)
+            .then((response) => {
+                if (response.data.success) {
+                    let dataArray = response.data.data;
+
+                    let dateLabels = [], dataToShow = [];
+                    let shift1 = [], shift2 = [], shift3 = [];
+
+                    dataArray.map(currentDay => {
+                        switch (currentDay['SHIFT_NO']) {
+                            case '1':
+                                shift1.push(currentDay['PRODUCTION_RATE']);
+                                dateLabels.push(currentDay['WORK_DATE']);
+                                break;
+                            case '2':
+                                shift2.push(currentDay['PRODUCTION_RATE']);
+                                break;
+                            case '3':
+                                shift3.push(currentDay['PRODUCTION_RATE']);
+                                break;
+                        }
+                    });
+
+                    dataToShow.push(shift1, shift2, shift3);
+
+                    console.log("DATE_LABELS_PRODUCTION_RATE", dateLabels);
+                    console.log("DATA_TO_SHOW_PRODUCTION_RATE", dataToShow);
+
+                    this.setState({
+                        ...this.state,
+                        dateLabels: dateLabels,
+                        productionRate: dataToShow,
+                    });
+                }
+            })
+            .catch((err) => console.log('err:', err));
+
+        API('api/os/defectByTypeOverTime', 'POST', param)
+            .then((response) => {
+                if (response.data.success) {
+                    let dataArray = response.data.data;
+
+                    let dateLabels = [], dataToShow = [];
+                    let defectType1 = [], defectType2 = [], defectType3 = [], defectType4 = [];
+
+                    dataArray.map(currentDay => {
+                        defectType1.push(currentDay['DEFECT_COUNT1']);
+                        defectType2.push(currentDay['DEFECT_COUNT2']);
+                        defectType3.push(currentDay['DEFECT_COUNT3']);
+                        defectType4.push(currentDay['DEFECT_COUNT4']);
+
+                        dateLabels.push(currentDay['WORK_DATE']);
+                    });
+
+                    dataToShow.push(defectType1, defectType2, defectType3, defectType4);
+
+                    console.log("DATE_LABELS_DEFECT_BY_TYPE", dateLabels);
+                    console.log("DATA_TO_SHOW_DEFECT_BY_TYPE", dataToShow);
+
+                    this.setState({
+                        ...this.state,
+                        dateLabels: dateLabels,
+                        defectByTypeOverTime: dataToShow,
+                    });
+                }
+            })
+            .catch((err) => console.log('err:', err));
     }
 
     toggle(tab) {

fc2399ba0bea633526709aeba0a70779918583a7
Minh-Tri Le <lmtri1995@gmail.com>
2019-03-08 12:14:22 +0700

    Show 0 data on Charts in Report Page on dates which have no data

--- frontend_chart/src/containers/Report/index.jsx
+++ frontend_chart/src/containers/Report/index.jsx
@@ -20,8 +20,9 @@ class ReportPage extends Component {
         this.toggle = this.toggle.bind(this);
         this.state = {
             activeTab: '1',
-            dateLabels: null,
+            productionRateDateLabels: null,
             productionRate: null,
+            defectRateDateLabels: null,
             defectByTypeOverTime: null,
         };
 
@@ -65,7 +66,6 @@ class ReportPage extends Component {
 
             this.requestProductionRates(param);
             this.requestDefectByTypeOverTime(param);
-
         }
     }
 
@@ -87,46 +87,65 @@ class ReportPage extends Component {
                 if (response.data.success) {
                     let dataArray = response.data.data;
 
-                    let dateLabels = [], dataToShow = [];
-                    let shift1 = [], shift2 = [], shift3 = [];
-                    let averageProductionRatesByDay = [];
+                    let {from_workdate, to_workdate} = param;
+                    let startMoment = moment(from_workdate, "YYYYMMDD");
+                    let endMoment = moment(to_workdate, "YYYYMMDD");
 
-                    let totalProductionRateOfCurrentDay = 0, currentProductionRate = 0;
-                    let averageProductionRate = 0;
-                    dataArray.map(currentDay => {
-                        currentProductionRate = currentDay['PRODUCTION_RATE'];
+                    let dateLabelsAndProductionRatesMap = new Map();
+                    while (startMoment.isSameOrBefore(endMoment)) {
+                        // array of 3 (zero) elements for 3 production rates of 3 shifts each day
+                        dateLabelsAndProductionRatesMap.set(startMoment.format('DD/MM/YYYY'), [0, 0, 0]);
+
+                        startMoment = startMoment.add(1, "days");
+                    }
+
+                    let shiftDataOfCurrentDay, currentProductionRate = 0;
+                    dataArray.map(currentData => {
+                        currentProductionRate = currentData['PRODUCTION_RATE'];
                         // Round to 2 decimal places
                         currentProductionRate = currentProductionRate % 1 === 0
                             ? currentProductionRate
                             : Math.round(currentProductionRate * 100) / 100;
 
-                        switch (currentDay['SHIFT_NO']) {
+                        switch (currentData['SHIFT_NO']) {
                             case '1':
-                                shift1.push(currentProductionRate);
-                                dateLabels.push(currentDay['WORK_DATE']);
-
-                                totalProductionRateOfCurrentDay = 0;
-                                totalProductionRateOfCurrentDay += currentProductionRate;
+                                shiftDataOfCurrentDay = dateLabelsAndProductionRatesMap.get(currentData['WORK_DATE']);
+                                shiftDataOfCurrentDay[0] = currentProductionRate;
+                                dateLabelsAndProductionRatesMap.set(currentData['WORK_DATE'], shiftDataOfCurrentDay);
                                 break;
                             case '2':
-                                shift2.push(currentProductionRate);
-
-                                totalProductionRateOfCurrentDay += currentProductionRate;
+                                shiftDataOfCurrentDay = dateLabelsAndProductionRatesMap.get(currentData['WORK_DATE']);
+                                shiftDataOfCurrentDay[1] = currentProductionRate;
+                                dateLabelsAndProductionRatesMap.set(currentData['WORK_DATE'], shiftDataOfCurrentDay);
                                 break;
                             case '3':
-                                shift3.push(currentProductionRate);
-
-                                totalProductionRateOfCurrentDay += currentProductionRate;
-
-                                averageProductionRate = totalProductionRateOfCurrentDay / 3;
-                                averageProductionRate = averageProductionRate % 1 !== 0
-                                    ? Math.round(averageProductionRate * 100) / 100
-                                    : averageProductionRate;
-                                averageProductionRatesByDay.push(averageProductionRate); // Average of 3 shifts
+                                shiftDataOfCurrentDay = dateLabelsAndProductionRatesMap.get(currentData['WORK_DATE']);
+                                shiftDataOfCurrentDay[2] = currentProductionRate;
+                                dateLabelsAndProductionRatesMap.set(currentData['WORK_DATE'], shiftDataOfCurrentDay);
                                 break;
                         }
                     });
 
+                    let dateLabels = [];
+                    let shift1 = [], shift2 = [], shift3 = [];
+                    let averageProductionRate = 0, averageProductionRatesByDay = [];
+                    dateLabelsAndProductionRatesMap.forEach((shiftData, date) => {
+                        dateLabels.push(date);
+
+                        shift1.push(shiftData[0]);
+                        shift2.push(shiftData[1]);
+                        shift3.push(shiftData[2]);
+
+                        averageProductionRate = (shiftData[0] + shiftData[1] + shiftData[2]) / 3;
+
+                        averageProductionRate = averageProductionRate % 1 !== 0
+                            ? Math.round(averageProductionRate * 100) / 100
+                            : averageProductionRate;
+
+                        averageProductionRatesByDay.push(averageProductionRate);    // Average of 3 shifts
+                    });
+
+                    let dataToShow = [];
                     // Colors = Shift 1 + Shift 2 + Shift 3 + Average line + Average point background color
                     let colors = ['#FF9C64', '#8C67F6', '#F575F7', '#EBEDF1', '#CCCCCC'];
                     for (let i = 1; i <= 4; ++i) {  // 3 Shifts' Production Rates + 1 Average Production Rates
@@ -160,7 +179,7 @@ class ReportPage extends Component {
 
                     this.setState({
                         ...this.state,
-                        dateLabels: dateLabels,
+                        productionRateDateLabels: dateLabels,
                         productionRate: dataToShow,
                     });
                 }
@@ -174,25 +193,45 @@ class ReportPage extends Component {
                 if (response.data.success) {
                     let dataArray = response.data.data;
 
-                    let dateLabels = [], dataToShow = [];
-                    let defectType1 = [], defectType2 = [], defectType3 = [], defectType4 = [];
-                    let totalDefectsByDay = [];
+                    let {from_workdate, to_workdate} = param;
+                    let startMoment = moment(from_workdate, "YYYYMMDD");
+                    let endMoment = moment(to_workdate, "YYYYMMDD");
 
-                    let totalDefectOfCurrentDay = 0, currentDefect = 0;
+                    let dateLabelsAndDefectRatesMap = new Map();
+                    while (startMoment.isSameOrBefore(endMoment)) {
+                        // array of 4 (zero) elements for 4 defect rates of 4 shifts each day
+                        dateLabelsAndDefectRatesMap.set(startMoment.format('DD/MM/YYYY'), [0, 0, 0, 0]);
+
+                        startMoment = startMoment.add(1, "days");
+                    }
+
+                    let defectRatesOfCurrentDay;
                     dataArray.map(currentDay => {
-                        for (let i = 1; i <= 4; ++i) {  // 4 Defect Types
-                            currentDefect = currentDay[`DEFECT_COUNT${i}`];
+                        defectRatesOfCurrentDay = dateLabelsAndDefectRatesMap.get(currentDay['WORK_DATE']);
 
-                            eval(`defectType${i}.push(currentDefect)`);
+                        defectRatesOfCurrentDay[0] = currentDay['DEFECT_COUNT1'];
+                        defectRatesOfCurrentDay[1] = currentDay['DEFECT_COUNT2'];
+                        defectRatesOfCurrentDay[2] = currentDay['DEFECT_COUNT3'];
+                        defectRatesOfCurrentDay[3] = currentDay['DEFECT_COUNT4'];
 
-                            totalDefectOfCurrentDay += currentDefect;
-                        }
-                        totalDefectsByDay.push(totalDefectOfCurrentDay);
-                        totalDefectOfCurrentDay = 0;
-
-                        dateLabels.push(currentDay['WORK_DATE']);
+                        dateLabelsAndDefectRatesMap.set(currentDay['WORK_DATE'], defectRatesOfCurrentDay);
                     });
 
+                    let dateLabels = [];
+                    let defectType1 = [], defectType2 = [], defectType3 = [], defectType4 = [];
+                    let totalDefectsByDay = [];
+                    dateLabelsAndDefectRatesMap.forEach((defectRates, date) => {
+                        dateLabels.push(date);
+
+                        defectType1.push(defectRates[0]);
+                        defectType2.push(defectRates[1]);
+                        defectType3.push(defectRates[2]);
+                        defectType4.push(defectRates[3]);
+
+                        totalDefectsByDay.push(defectRates[0] + defectRates[1] + defectRates[2] + defectRates[3]);
+                    });
+
+                    let dataToShow = [];
                     // Colors = Type 1 + Type 2 + Type 3 + Type 4 + Total Defect line + Total Defect point background color
                     let colors = ['#FF9C64', '#46D6EA', '#F575F7', '#8C67F6', '#EB6A91', '#EBEDF1'];
                     for (let i = 1; i <= 5; ++i) {  // 4 Defect Types + 1 Total Defect
@@ -226,7 +265,7 @@ class ReportPage extends Component {
 
                     this.setState({
                         ...this.state,
-                        dateLabels: dateLabels,
+                        defectRateDateLabels: dateLabels,
                         defectByTypeOverTime: dataToShow,
                     });
                 }
@@ -265,7 +304,7 @@ class ReportPage extends Component {
                     <TabPane tabId="1">
                         <div className="row">
                             <div className="col-9">
-                                <ProductionRate labels={this.state.dateLabels}
+                                <ProductionRate labels={this.state.productionRateDateLabels}
                                                 productionRate={this.state.productionRate}/>
                             </div>
                             <div className="col-3">
@@ -276,7 +315,7 @@ class ReportPage extends Component {
                     <TabPane tabId="2">
                         <div className="row">
                             <div className="col-9">
-                                <DefectRate labels={this.state.dateLabels}
+                                <DefectRate labels={this.state.defectRateDateLabels}
                                             defectByTypeOverTime={this.state.defectByTypeOverTime}/>
                             </div>
                             <div className="col-3">

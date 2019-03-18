b77663a2fe641e498170f63bb793704d5f1d0123
truong ho <truong.ho.it@gmail.com>
2019-03-18 13:15:41 +0700

    Fill OEE Number, Lost of work for analysis

--- frontend_chart/src/containers/Analysis/components/ListOfBottomComponent/components/OEEGeneral.jsx
+++ frontend_chart/src/containers/Analysis/components/ListOfBottomComponent/components/OEEGeneral.jsx
@@ -1,13 +1,15 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {changeNumberFormat} from "../../../../../shared/utils/Utilities";
 
 export class OEEGeneral extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {};
     }
 
     render() {
+        let {OEENumber} = this.props;
         return (
             <div className="oee-main">
                 <div className="col-12"><h4>OEE</h4></div>
@@ -15,7 +17,7 @@ export class OEEGeneral extends Component {
                     <div className="container">
                         <div className="row">
                             <div className="col align-self-center"><i
-                                className="fas fa-arrow-up"></i><span>67.5%</span></div>
+                                className="fas fa-arrow-up"></i><span>{OEENumber}</span></div>
                         </div>
                     </div>
                 </div>

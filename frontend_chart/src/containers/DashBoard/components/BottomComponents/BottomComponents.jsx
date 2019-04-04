03df9b344065bdb1aca25039de030eea844f168a
Tri Le <lmtri1995@gmail.com>
2019-04-04 17:39:55 +0700

    Edit Quality Calculations for OEE Components

--- frontend_chart/src/containers/DashBoard/components/BottomComponents/BottomComponents.jsx
+++ frontend_chart/src/containers/DashBoard/components/BottomComponents/BottomComponents.jsx
@@ -151,29 +151,39 @@ class BottomComponents extends Component {
         this.countCurrentWorkingHour();
 
         let stoppingHour1 = 0, productCount1 = 0, preparingTime1 = 0,
-            cycleCount1 = 0, defect1 = 0,
+            cycleCount1 = 0,
+            // defect1 = 0,
             standardCycleTime1 = this.standardCycleTimeArray[0];
         let stoppingHour2 = 0, productCount2 = 0, preparingTime2 = 0,
-            cycleCount2 = 0, defect2 = 0,
+            cycleCount2 = 0,
+            // defect2 = 0,
             standardCycleTime2 = this.standardCycleTimeArray[1];
         let stoppingHour3 = 0, productCount3 = 0, preparingTime3 = 0,
-            cycleCount3 = 0, defect3 = 0,
+            cycleCount3 = 0,
+            // defect3 = 0,
             standardCycleTime3 = this.standardCycleTimeArray[2];
         let stoppingHour4 = 0, productCount4 = 0, preparingTime4 = 0,
-            cycleCount4 = 0, defect4 = 0,
+            cycleCount4 = 0,
+            // defect4 = 0,
             standardCycleTime4 = this.standardCycleTimeArray[3];
         let stoppingHour5 = 0, productCount5 = 0, preparingTime5 = 0,
-            cycleCount5 = 0, defect5 = 0,
+            cycleCount5 = 0,
+            // defect5 = 0,
             standardCycleTime5 = this.standardCycleTimeArray[4];
         let stoppingHour6 = 0, productCount6 = 0, preparingTime6 = 0,
-            cycleCount6 = 0, defect6 = 0,
+            cycleCount6 = 0,
+            // defect6 = 0,
             standardCycleTime6 = this.standardCycleTimeArray[5];
         let stoppingHour7 = 0, productCount7 = 0, preparingTime7 = 0,
-            cycleCount7 = 0, defect7 = 0,
+            cycleCount7 = 0,
+            // defect7 = 0,
             standardCycleTime7 = this.standardCycleTimeArray[6];
         let stoppingHour8 = 0, productCount8 = 0, preparingTime8 = 0,
-            cycleCount8 = 0, defect8 = 0,
+            cycleCount8 = 0,
+            // defect8 = 0,
             standardCycleTime8 = this.standardCycleTimeArray[7];
+
+        let totalDefect = 0;
 
         if (dataArray && dataArray.length > 0) {
             dataArray.map(item => {
@@ -181,116 +191,128 @@ class BottomComponents extends Component {
                 let count = item.count ? item.count : 0;
                 let preparingtime = item.preparingtime ? item.preparingtime : 0;
                 let cycle_count = item.cycle_count ? item.cycle_count : 0;
-                let defect = item.defect ? item.defect : 0;
+                totalDefect = item.defect ? item.defect : 0;
                 if (item.idStation == 1) {
                     stoppingHour1 += parseFloat(stopping_hr);
                     productCount1 += parseInt(count);
                     preparingTime1 += parseInt(preparingtime);
                     cycleCount1 += parseInt(cycle_count);
-                    defect1 += parseInt(defect);
+                    // defect1 += parseInt(defect);
                 } else if (item.idStation == 2) {
                     stoppingHour2 += parseFloat(stopping_hr);
                     productCount2 += parseInt(count);
                     preparingTime2 += parseInt(preparingtime);
                     cycleCount2 += parseInt(cycle_count);
-                    defect2 += parseInt(defect);
+                    // defect2 += parseInt(defect);
                 } else if (item.idStation == 3) {
                     stoppingHour3 += parseFloat(stopping_hr);
                     productCount3 += parseInt(count);
                     preparingTime3 += parseInt(preparingtime);
                     cycleCount3 += parseInt(cycle_count);
-                    defect3 += parseInt(defect);
+                    // defect3 += parseInt(defect);
                 } else if (item.idStation == 4) {
                     stoppingHour4 += parseFloat(stopping_hr);
                     productCount4 += parseInt(count);
                     preparingTime4 += parseInt(preparingtime);
                     cycleCount4 += parseInt(cycle_count);
-                    defect4 += parseInt(defect);
+                    // defect4 += parseInt(defect);
                 } else if (item.idStation == 5) {
                     stoppingHour5 += parseFloat(stopping_hr);
                     productCount5 += parseInt(count);
                     preparingTime5 += parseInt(preparingtime);
                     cycleCount5 += parseInt(cycle_count);
-                    defect5 += parseInt(defect);
+                    // defect5 += parseInt(defect);
                 } else if (item.idStation == 6) {
                     stoppingHour6 += parseFloat(stopping_hr);
                     productCount6 += parseInt(count);
                     preparingTime6 += parseInt(preparingtime);
                     cycleCount6 += parseInt(cycle_count);
-                    defect6 += parseInt(defect);
+                    // defect6 += parseInt(defect);
                 } else if (item.idStation == 7) {
                     stoppingHour7 += parseFloat(stopping_hr);
                     productCount7 += parseInt(count);
                     preparingTime7 += parseInt(preparingtime);
                     cycleCount7 += parseInt(cycle_count);
-                    defect7 += parseInt(defect);
+                    // defect7 += parseInt(defect);
                 } else if (item.idStation == 8) {
                     stoppingHour8 += parseFloat(stopping_hr);
                     productCount8 += parseInt(count);
                     preparingTime8 += parseInt(preparingtime);
                     cycleCount8 += parseInt(cycle_count);
-                    defect8 += parseInt(defect);
+                    // defect8 += parseInt(defect);
                 }
             });
         }
 
         let availability1 = (this.totalWorkingHour - stoppingHour1) / this.totalWorkingHour,
             performance1 = (standardCycleTime1 * productCount1) / (this.totalWorkingHour - stoppingHour1),
-            quality1 = (productCount1 - defect1) / productCount1,
-            OEE1 = availability1 * performance1 * quality1,
+            // quality1 = (productCount1 - defect1) / productCount1,
+            // OEE1 = availability1 * performance1 * quality1,
+            OEE1 = availability1 * performance1,
             workLost1 = preparingTime1 / (standardCycleTime1 * cycleCount1);
 
         let availability2 = (this.totalWorkingHour - stoppingHour2) / this.totalWorkingHour,
             performance2 = (standardCycleTime2 * productCount2) / (this.totalWorkingHour - stoppingHour2),
-            quality2 = (productCount2 - defect2) / productCount2,
-            OEE2 = availability2 * performance2 * quality2,
+            // quality2 = (productCount2 - defect2) / productCount2,
+            // OEE2 = availability2 * performance2 * quality2,
+            OEE2 = availability2 * performance2,
             workLost2 = preparingTime2 / (standardCycleTime2 * cycleCount2);
 
         let availability3 = (this.totalWorkingHour - stoppingHour3) / this.totalWorkingHour,
             performance3 = (standardCycleTime3 * productCount3) / (this.totalWorkingHour - stoppingHour3),
-            quality3 = (productCount3 - defect3) / productCount3,
-            OEE3 = availability3 * performance3 * quality3,
+            // quality3 = (productCount3 - defect3) / productCount3,
+            // OEE3 = availability3 * performance3 * quality3,
+            OEE3 = availability3 * performance3,
             workLost3 = preparingTime3 / (standardCycleTime3 * cycleCount3);
 
         let availability4 = (this.totalWorkingHour - stoppingHour4) / this.totalWorkingHour,
             performance4 = (standardCycleTime4 * productCount4) / (this.totalWorkingHour - stoppingHour4),
-            quality4 = (productCount4 - defect4) / productCount4,
-            OEE4 = availability4 * performance4 * quality4,
+            // quality4 = (productCount4 - defect4) / productCount4,
+            // OEE4 = availability4 * performance4 * quality4,
+            OEE4 = availability4 * performance4,
             workLost4 = preparingTime4 / (standardCycleTime4 * cycleCount4);
 
         let availability5 = (this.totalWorkingHour - stoppingHour5) / this.totalWorkingHour,
             performance5 = (standardCycleTime5 * productCount5) / (this.totalWorkingHour - stoppingHour5),
-            quality5 = (productCount5 - defect5) / productCount5,
-            OEE5 = availability5 * performance5 * quality5,
+            // quality5 = (productCount5 - defect5) / productCount5,
+            // OEE5 = availability5 * performance5 * quality5,
+            OEE5 = availability5 * performance5,
             workLost5 = preparingTime5 / (standardCycleTime5 * cycleCount5);
 
         let availability6 = (this.totalWorkingHour - stoppingHour6) / this.totalWorkingHour,
             performance6 = (standardCycleTime6 * productCount6) / (this.totalWorkingHour - stoppingHour6),
-            quality6 = (productCount6 - defect6) / productCount6,
-            OEE6 = availability6 * performance6 * quality6,
+            // quality6 = (productCount6 - defect6) / productCount6,
+            // OEE6 = availability6 * performance6 * quality6,
+            OEE6 = availability6 * performance6,
             workLost6 = preparingTime6 / (standardCycleTime6 * cycleCount6);
 
         let availability7 = (this.totalWorkingHour - stoppingHour7) / this.totalWorkingHour,
             performance7 = (standardCycleTime7 * productCount7) / (this.totalWorkingHour - stoppingHour7),
-            quality7 = (productCount7 - defect7) / productCount7,
-            OEE7 = availability7 * performance7 * quality7,
+            // quality7 = (productCount7 - defect7) / productCount7,
+            // OEE7 = availability7 * performance7 * quality7,
+            OEE7 = availability7 * performance7,
             workLost7 = preparingTime7 / (standardCycleTime7 * cycleCount7);
 
         let availability8 = (this.totalWorkingHour - stoppingHour8) / this.totalWorkingHour,
             performance8 = (standardCycleTime8 * productCount8) / (this.totalWorkingHour - stoppingHour8),
-            quality8 = (productCount8 - defect8) / productCount8,
-            OEE8 = availability8 * performance8 * quality8,
+            // quality8 = (productCount8 - defect8) / productCount8,
+            // OEE8 = availability8 * performance8 * quality8,
+            OEE8 = availability8 * performance8,
             workLost8 = preparingTime8 / (standardCycleTime8 * cycleCount8);
 
+        let totalProductCount = productCount1 + productCount2 + productCount3 + productCount4 +
+            productCount5 + productCount6 + productCount7 + productCount8;
+        let overallQuality = ((totalProductCount) - totalDefect) / totalProductCount;
+
         let summaryArray = [
-            [availability1, performance1, quality1, OEE1, workLost1],
-            [availability2, performance2, quality2, OEE2, workLost2],
-            [availability3, performance3, quality3, OEE3, workLost3],
-            [availability4, performance4, quality4, OEE4, workLost4],
-            [availability5, performance5, quality5, OEE5, workLost5],
-            [availability6, performance6, quality6, OEE6, workLost6],
-            [availability7, performance7, quality7, OEE7, workLost7],
-            [availability8, performance8, quality8, OEE8, workLost8]
+            [availability1, performance1, overallQuality, OEE1, workLost1],
+            [availability2, performance2, overallQuality, OEE2, workLost2],
+            [availability3, performance3, overallQuality, OEE3, workLost3],
+            [availability4, performance4, overallQuality, OEE4, workLost4],
+            [availability5, performance5, overallQuality, OEE5, workLost5],
+            [availability6, performance6, overallQuality, OEE6, workLost6],
+            [availability7, performance7, overallQuality, OEE7, workLost7],
+            [availability8, performance8, overallQuality, OEE8, workLost8]
         ];
         return summaryArray;
     }
@@ -397,19 +419,19 @@ class BottomComponents extends Component {
                 try {
                     let data = response.data.data;
                     let summaryArray = this.handleReturnArray(data);
-                    let availability = 0, performance = 0, quality = 0, OEE = 0, workLost = 0;
+                    let availability = 0, performance = 0, overallQuality = 0, OEE = 0, workLost = 0;
                     summaryArray.map(item => {
                         availability += (item[0] ? item[0] : 0);
                         performance += (item[1] ? item[1] : 0);
-                        quality += (item[2] ? item[2] : 0);
+                        overallQuality = (item[2] ? item[2] : 0);
                         OEE += (item[3] ? item[3] : 0);
                         workLost += (item[4] ? item[4] : 0);
                     });
                     this.setState({
                         availabilityNumber: Math.round(((availability / summaryArray.length) * 100) * 100) / 100,
                         performanceNumber: Math.round(((performance / summaryArray.length) * 100) * 100) / 100,
-                        qualityNumber: Math.round(((quality / summaryArray.length) * 100) * 100) / 100,
-                        OEENumber: Math.round(((OEE / summaryArray.length) * 100) * 100) / 100,
+                        qualityNumber: Math.round((overallQuality * 100) * 100) / 100,
+                        OEENumber: Math.round(((OEE / summaryArray.length) * overallQuality * 100) * 100) / 100,
                         workLossNumber: Math.round(((workLost / summaryArray.length) * 100) * 100) / 100,
                         loading: false,
                     });
@@ -464,19 +486,19 @@ class BottomComponents extends Component {
                         let data = returnData.data;
                         let summaryArray = this.handleReturnArray(data);
 
-                        let availability = 0, performance = 0, quality = 0, OEE = 0, workLost = 0;
+                        let availability = 0, performance = 0, overallQuality = 0, OEE = 0, workLost = 0;
                         summaryArray.map(item => {
                             availability += (item[0] ? item[0] : 0);
                             performance += (item[1] ? item[1] : 0);
-                            quality += (item[2] ? item[2] : 0);
+                            overallQuality = (item[2] ? item[2] : 0);
                             OEE += (item[3] ? item[3] : 0);
                             workLost += (item[4] ? item[4] : 0);
                         });
                         this.setState({
                             availabilityNumber: Math.round(((availability / summaryArray.length) * 100) * 100) / 100,
                             performanceNumber: Math.round(((performance / summaryArray.length) * 100) * 100) / 100,
-                            qualityNumber: Math.round(((quality / summaryArray.length) * 100) * 100) / 100,
-                            OEENumber: Math.round(((OEE / summaryArray.length) * 100) * 100) / 100,
+                            qualityNumber: Math.round((overallQuality * 100) * 100) / 100,
+                            OEENumber: Math.round(((OEE / summaryArray.length) * overallQuality * 100) * 100) / 100,
                             workLossNumber: Math.round(((workLost / summaryArray.length) * 100) * 100) / 100,
                             loading: false,
                         });

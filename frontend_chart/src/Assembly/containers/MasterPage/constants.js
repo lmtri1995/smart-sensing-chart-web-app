0473441e8ee9c8bb1edc91ef91d7c8b8398da63d
truong ho <truong.ho.it@gmail.com>
2019-10-31 15:27:49 +0700

    Change password to unlock submit button to ilovehsvina

--- frontend_chart/src/Assembly/containers/MasterPage/constants.js
+++ frontend_chart/src/Assembly/containers/MasterPage/constants.js
@@ -49,7 +49,7 @@ const MASTER_FORM_CONSTANTS = {
 		onGoing: 1,
 		done   : 2,
 	},
-	enableSubmitPassword: 'hsvina123',
+	enableSubmitPassword: 'ilovehsvina',
 };
 
 export default MASTER_FORM_CONSTANTS;
